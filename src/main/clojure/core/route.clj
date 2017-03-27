5a76a502a80d79d6d76cf442ede17cc818e01ace
tuzi_li <bobotu@1122.moe>
2017-03-27 21:52:42 +0800

    CaffèHub 报名

--- src/main/clojure/core/route.clj
+++ src/main/clojure/core/route.clj
@@ -1,109 +1,37 @@
 (ns core.route
   (:use [ring.util.servlet :only (defservice)]
         compojure.core
+        compojure.route
         core.sign-up
         core.util
         ring.middleware.json
         ring.middleware.keyword-params
-        ring.middleware.multipart-params
-        ring.middleware.multipart-params.byte-array
-        ring.util.response
+        [ring.util.response :exclude (not-found)]
         clojure.tools.logging)
   (:import [com.avos.avoscloud AVUser])
   (:gen-class
     :extends javax.servlet.http.HttpServlet))
-
-(defn- wrap-check-login
-  [handler]
-  (fn [request]
-    (if-let [token (get (:params request) :token (get (:params request) "token"))]
-      (if-let [user (AVUser/becomeWithSessionToken token)]
-        (handler (assoc-in request [:params :user] user))
-        (-> (response "error")
-            (status 401)))
-      (-> (response "error")
-          (status 401)))))
 
 (def wrap-json-params-routes
   (comp wrap-json-response
         wrap-json-params
         wrap-keyword-params))
 
-(def wrap-json-body-routes
-  (comp wrap-json-response
-        wrap-json-body))
-
-(def ^:private allowed-origin
-  #{"https://www.xdmsc.club"
-    "http://www.xdmsc.club"
-    "https://xdmsc.club"
-    "http://xdmsc.club"
-    "http://127.0.0.1:8080"})
-
-(def ^:private allowed-method #{"GET" "POST"})
-
-(def ^:private allowed-header #{"Content-Type"})
-
 (defroutes
   sign-up-routes*
-  (context "/join" []
-    (-> (POST "/sign-up" [username password]
-          (response (sign-up username password)))
-        (wrap-routes wrap-json-params-routes))
+  (-> (POST "/sign-up" [id name phone]
+        (response (sign-up id name phone)))
+      (wrap-routes wrap-json-params-routes))
 
-    (-> (POST "/verify-phone" [code]
+  (-> (POST "/verify-phone" [code]
           (response (verify-phone code)))
-        (wrap-routes wrap-json-params-routes))
+      (wrap-routes wrap-json-params-routes))
 
-    (-> (POST "/request-verify" [phone]
-          (response (request-phone-verify phone)))
-        (wrap-routes (comp wrap-json-params-routes)))
+  (-> (POST "/request-phone-verify" [phone]
+        (response (request-phone-verify phone)))
+      (wrap-routes wrap-json-params-routes))
 
-    (-> (POST "/set-base-info" [user data]
-          (response (set-base-info data user)))
-        (wrap-routes (comp wrap-json-params-routes
-                           wrap-check-login)))
+  (GET "/" [] (resource-response "index.html")))
 
-    (-> (POST "/set-sign-up-info" [user data]
-          (response (set-sign-up-info data user)))
-        (wrap-routes (comp wrap-json-params-routes
-                           wrap-check-login)))
-
-    (-> (POST "/user-exist" [email]
-          (response (user-exist? email)))
-        (wrap-routes wrap-json-params-routes))
-
-    (-> (POST "/login" [email password]
-          (response (log-in email password)))
-        (wrap-routes wrap-json-params-routes))
-
-    (-> (POST "/sign-up-info" [user]
-          (response (get-sign-up-info user)))
-        (wrap-routes (comp wrap-json-params-routes
-                           wrap-check-login)))
-
-    (-> (POST "/base-info" [user]
-          (response (get-base-info user)))
-        (wrap-routes (comp wrap-json-params-routes
-                           wrap-check-login)))
-
-    (-> (POST "/file" [file user :as req]
-          (response (save-file file user)))
-        (wrap-routes (comp wrap-json-response
-                           #(wrap-multipart-params % {:store (byte-array-store)})
-                           wrap-check-login)))
-
-    (-> (POST "/delete-file" [id user]
-          (response (delete-file id user)))
-        (wrap-routes (comp wrap-json-params-routes
-                           wrap-check-login)))
-
-    (OPTIONS "*" []
-      (response ""))))
-
-(def sign-up-routes
-  (wrap-cors sign-up-routes* allowed-origin allowed-method allowed-header))
-
-(defservice (routes
-              (ANY "*" [] (-> (response "notfound")
-                              (status 404)))))
+(defservice (routes sign-up-routes*
+                    (not-found "404 Not Found")))
--- src/main/clojure/core/sign_up.clj
+++ src/main/clojure/core/sign_up.clj
@@ -15,53 +15,9 @@
   `(try
      ~@body
      (catch AVException e#
-       (error (.getCode e#))
-       {:success? false :error (.getMessage e#)})))
-
-(defn user-exist?
-  "给定邮箱查看用户是否存在"
-  [username]
-  (try
-    {:success? true
-     :exist?   (-> (client/get "https://api.leancloud.cn/1.1/users"
-                               {:headers      {"X-LC-Id"   (System/getenv "LEANCLOUD_APP_ID")
-                                               "X-LC-Sign" (get-sign :master)}
-                                :as           :json
-                                :content-type :json
-                                :accept       :json
-                                :query-params {"where" (write-str {:username username})
-                                               "limit" 1}})
-                   (:body)
-                   (:results)
-                   (#(or (empty? %) (nil? %)))
-                   (not))}
-    (catch Exception e
-      {:success? false})))
-
-(defn sign-up
-  [^String e-mail ^String password]
-  (wrap-try
-    (let [token (.getSessionToken (doto (AVUser.)
-                                    (.setUsername e-mail)
-                                    (.setEmail e-mail)
-                                    (.setPassword password)
-                                    (.signUp)))]
-      {:success? true :token token})))
-
-(defn log-in
-  [^String e-mail ^String password]
-  (wrap-try
-    {:success? true :token (.getSessionToken (AVUser/logIn e-mail password))}))
-
-(defn set-phone
-  [^String phone]
-  (wrap-try
-    (let [user (AVUser/getCurrentUser)]
-      (if-not (.getBoolean (.fetch user) "mobilePhoneVerified")
-        (do
-          (.setMobilePhoneNumber user phone)
-          (.save user)))
-      {:success? true})))
+       (if (= 214 (.getCode e#))
+         {:success? true :exist? true}
+         {:success? false :error (.getMessage e#)}))))
 
 (defn request-phone-verify
   [^String phone]
@@ -75,94 +31,14 @@
     (AVUser/verifyMobilePhone code)
     {:success? true}))
 
-(defn set-base-info
-  [info ^AVUser user]
+(defn sign-up
+  [^String id ^String name ^String phone]
   (wrap-try
-    (let [phone (info :phone)
-          info (dissoc info :phone)]
-      (-> user
-          (put-object info)
-          (save-object))
-      (set-phone phone)
-      {:success? true})))
-
-(defn set-sign-up-info
-  [info ^AVUser user]
-  (wrap-try
-    (let [form (-> (avos-query "SignUp")
-                   (query-object ["user" := user])
-                   (query-first))]
-      (if-not (nil? form)
-        (do
-          (-> (put-object form info)
-              (save-object))
-          {:success? true})
-        (do
-          (-> (avos-object "SignUp")
-              (put-object info)
-              (put-object ["user" user])
-              (save-object))
-          {:success? true :files []})))))
-
-(defn get-sign-up-info
-  [^AVUser user]
-  (wrap-try
-    (try
-      (let [info ^AVObject (-> (avos-query "SignUp")
-                               (query-object ["user" := user])
-                               (query-include "files")
-                               (query-first))
-            files (:files (get-slot info ["files" :seq]))
-            files (map (fn [file] {:name    (file "originalName")
-                                   :url     (file "url")
-                                   :id      (file "objectId")
-                                   :loading false}) files)]
-        (-> (.toJSONObject info)
-            (.toString)
-            (read-str)
-            (assoc "files" files)
-            (assoc "success?" true)
-            (dissoc "className" "createdAt" "objectId" "updatedAt" "user")))
-      (catch NullPointerException e
-        {:success? true}))))
-
-(defn get-base-info
-  [^AVUser user]
-  (wrap-try
-    (-> user
-        (.toJSONObject)
-        (.toString)
-        (read-str)
-        (dissoc "className" "emailVerified" "createdAt" "sessionToken" "objectId" "email" "username" "updatedAt" "webpointer")
-        (assoc "success?" true))))
-
-(defn save-file
-  [file ^AVUser user]
-  (wrap-try
-    (let [name (:filename file)
-          file ^AVFile (save-file-bytes name (:bytes file))
-          info (-> (avos-query "SignUp")
-                   (query-object ["user" := user])
-                   (query-first))
-          info (if (nil? info) (-> (avos-object "SignUp")
-                                   (put-object ["user" user])
-                                   (save-object))
-                               info)]
-      (.add info "files" file)
-      (save-object info)
-      {:success? true
-       :name     name
-       :id       (.getObjectId file)
-       :url      (.getUrl file)})))
-
-(defn delete-file
-  [^String id ^AVUser user]
-  (wrap-try
-    (let [file ^AVFile (-> (avos-query "_File")
-                           (.get id))
-          user (-> (avos-query "SignUp")
-                   (query-object ["user" := user])
-                   (query-first))]
-      (.removeAll user "files" [file])
-      (save-object user)
-      {:success? true})))
+    (.getSessionToken (doto (AVUser.)
+                        (.setUsername id)
+                        (.setMobilePhoneNumber phone)
+                        (.setPassword phone)
+                        (.put "name" name)
+                        (.signUp)))
+    (future (request-phone-verify phone))
+    {:success? true :exist? false}))
